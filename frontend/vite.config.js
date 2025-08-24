29bb2f0f9c7de0484203d1e419c25d3c4556f773
atharva <baodhankaratharva@gmail.com>
2025-08-24 09:33:48 +0530

    bug fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load environment variables from .env file based on the current mode
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react(), tailwindcss()],
     define: {
       // Expose environment variables to your client-side code
-      'process.env': JSON.stringify(env),
+      "process.env": JSON.stringify(env),
     },
   };
 });
